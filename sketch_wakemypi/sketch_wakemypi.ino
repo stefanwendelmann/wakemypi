ee9daf4ec4ef0e27a7348198666d3a21b1af44f9
Stefan <stefanwendelmann@gmail.com>
2018-03-31 16:36:11 +0200

    First Sketch

---
+++ sketch_wakemypi/sketch_wakemypi.ino
@@ -0,0 +1,32 @@
+void setup() {
+  Serial.begin(9600);
+  // put your setup code here, to run once:
+  pinMode(2, OUTPUT);
+  Serial.println("ON");
+}
+
+int x;
+String s;
+
+void loop() {
+if(Serial.available()>0){
+  s = Serial.readString();
+  if(s.substring(0,5) == "TIME="){
+    setTime();
+  }else if(s.substring(0,7) == "WAKEIN="){
+    wakeIn();
+  }
+  Serial.println(s);
+}
+}
+
+void setTime(){
+  Serial.println("TIME SET TO = "+s.substring(5));
+  Serial.println("OK");
+}
+
+void wakeIn(){
+  Serial.println("WAKE YOU IN = "+s.substring(7)+" ms");
+  Serial.println("OK");
+}
+
