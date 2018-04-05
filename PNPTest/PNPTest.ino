bae279fb747fcb72e1c1c642aed7d9e7be3ac36b
Stefan Wendelmann <SWendelmann@itout.de>
2018-04-05 13:31:58 +0200

    Test um 4N35 erweitert

--- PNPTest/PNPTest.ino
+++ PNPTest/PNPTest.ino
@@ -1,10 +1,12 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
+  pinMode(2, OUTPUT); // LED
+  pinMode(3, OUTPUT); // PNP Transistor 
+  pinMode(4, OUTPUT); // Optokopller 4N35
   digitalWrite(2, HIGH);
   digitalWrite(3, HIGH);
+  digitalWrite(4, LOW);
   Serial.println("ON");
   
 }
@@ -13,9 +15,11 @@ void loop() {
   // RESET RPi 
   digitalWrite(2, LOW);
   digitalWrite(3, LOW);
+  digitalWrite(4, HIGH);
   delay(2000);
   // Let RPi GO
   digitalWrite(2, HIGH);
   digitalWrite(3, HIGH);
+  digitalWrite(4, LOW);
   delay(20000);
 }
