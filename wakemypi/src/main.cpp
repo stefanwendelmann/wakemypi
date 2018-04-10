18440d3e33f1aa5b0bb9f445314c3104624978e0
Stefan Wendelmann <SWendelmann@itout.de>
2018-04-10 13:51:58 +0200

    Validate Wake Delay and String to Long conversion

--- wakemypi/src/main.cpp
+++ wakemypi/src/main.cpp
@@ -11,10 +11,25 @@ void setup() {
       Serial.println("ON");
 }
 
+bool isValidWakeDelay(String str){
+  for(unsigned int i = 0; i<str.length();i++){
+    if(!isDigit(str.charAt(i))){
+      return false;
+    }
+  }
+  return true;
+}
+
 void wakeIn(){
   Serial.println("WAKE YOU IN = "+s.substring(7)+" ms");
+  if(!isValidWakeDelay(s.substring(7))){
+    Serial.println("ERROR: "+s.substring(7)+" IS NOT A VALID WAKE UP DELAY!");
+    return;
+  }
+
+  long d = s.substring(7).toInt();
   Serial.println("OK");
-  //delay()
+  delay(d);
   Serial.println("HEYWAKEUP!");
   digitalWrite(2, HIGH);
   digitalWrite(3, HIGH);
